8363ca3a97d6d92dbc3ba513a3e2355ddb430207
shubhamkumar9798 <149182027+shubhamkumar9798@users.noreply.github.com>
2024-10-16 13:37:29 +0000

    binded the host

--- app.py
+++ app.py
@@ -69,5 +69,5 @@ def suggestions(stress_level):
 
     return render_template('suggestion.html', advice=advice)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host= "0.0.0.0" ,debug=True)
